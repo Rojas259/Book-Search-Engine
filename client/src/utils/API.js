96ca8af4a8f761cdad594fcf816e7d0fe413648a
Rojas259 <147790485+Rojas259@users.noreply.github.com>
2024-04-08 16:25:33 -0700

    fixed connections

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -49,8 +49,6 @@ export const deleteBook = (bookId, token) => {
   });
 };
 
-// make a search to google books api
-// https://www.googleapis.com/books/v1/volumes?q=harry+potter
 export const searchGoogleBooks = (query) => {
   return fetch(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
 };
--- server/Schemas/index.js
+++ server/Schemas/index.js
@@ -1,4 +1,4 @@
-const typeDefs = require('./typeDefs');
+const typeDefs = require('./typesDefs');
 const resolvers = require('./resolvers');
 
 module.exports = { typeDefs, resolvers };
